2e4e451684432a39a0d611b8ffa073173c373a7d
bots-of-charles <116918675+bots-of-charles@users.noreply.github.com>
2022-12-13 16:41:52 -0500

    Add files via upload

--- search_mfshell.js
+++ search_mfshell.js
@@ -6,5 +6,11 @@ const kwdata={
 	"cmd_cd":"cd cwd dir directorio subdirectorio carpeta subcarpeta folder directory subdirectory parent change cambiar moverse move another otro",
 	"cmd_back":"back cwd dir directorio directory carpeta folder volver atrás atras change cambiar moverse move up",
 	"cmd_adl":"adl ls files archivos cwd dir directorio subdirectorio carpeta subcarpeta folder directory subdirectory parent listar cwd enumerar listing enumerate list lista ver mirar look auto automatic automated automático automatico automaticamente automáticamente automatizar self after switch activar activate",
+	"cmd_mkdir":"mkdir cwd dir nuevo new directorio directory carpeta folder crear create subdirectorio subdirectory",
+	"cmd_mv":"mv dir directorio directory carpeta folder subdirectorio subdirectory archivo file mover renombrar cambiar nombre move rename renaming change name filename",
+	"cmd_rm":"rm dir directorio directory carpeta folder subdirectorio subdirectory archivo file borrar eliminar delete remove eliminate erase",
+	"cmd_bren":"bren mv archivos files renombrar cambiar nombres rename renaming change names filenames múltiples multiple batch masa massive group",
+	"cmd_ext":"ext archivos files 7z zip rar xz tar tgz tar.xz tar.gz winrar extraer extract descomprimir decompress",
+	"cmd_join":"join archivos files concatenar concatenate unir merge merging cat raw",
 	"cmd_info":"info telegram file archivo properties propiedades information informacion información",
 }
