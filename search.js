a253035362df6349ea1269e7131e5dc1473c4af6
bots-of-charles <116918675+bots-of-charles@users.noreply.github.com>
2022-12-13 14:43:14 -0500

    Add files via upload

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
 const thing_coll=document.getElementsByClassName("thing");
 
-function search_matches(wlist_test,wlist_reference)
+function search_match_keywords(wlist_test,wlist_reference)
 {
 	let matches=0;
 	let idx=0;
@@ -48,27 +48,27 @@ function search_now()
 		let idx_max=thing_coll.length;
 		while (idx<idx_max)
 		{
+			let matches=0;
 			let thing=thing_coll[idx];
 			let thing_text=thing.getElementsByClassName("the_text")[0];
 
 			let data_kwords=kwdata[thing_text.id].split(" ");
 			data_kwords.push("/"+data_kwords[0]);
-			let thing_matches_kwords=search_matches(search_txtsplit,data_kwords);
+			matches=matches+search_match_keywords(search_txtsplit,data_kwords);
 
+			/*
 			let data_ftext=thing_text.innerText.toLowerCase().trim().replaceAll("\n",".").split(" ");
-			let thing_matches_ftext=search_matches(search_txtsplit,data_ftext);
-
+			let thing_matches_ftext=search_match_keywords(search_txtsplit,data_ftext);
+			*/
 			// console.log(thing_text.id+"\n\t"+thing_matches_ftext+"/"+data_ftext.length+"\n\t"+thing_matches_kwords+"/"+data_kwords.length)
 
-			if (thing_matches_kwords>0 || thing_matches_ftext>0) 
+			if (matches>0) 
 			{
-				results_raw.push({id:thing_text.id,score:thing_matches_kwords+thing_matches_ftext});
+				results_raw.push({id:thing_text.id,score:matches});
 			};
 
 			idx++;
 		};
-
-		// TODO: sort by score
 
 		idx=0;
 		while (idx<results_raw.length)
@@ -76,7 +76,16 @@ function search_now()
 			let elem_id=results_raw[idx].id;
 			let elem_text=document.getElementById(elem_id).parentElement.innerHTML;
 			let results_section=document.getElementById("search_results");
-			results_section.innerHTML=results_section.innerHTML+"\n\t\t\t\t\t<div class=\"thing\">\n"+elem_text+"\n\t\t\t\t\t</div>";
+
+			let score_display="";
+			let i=0;
+			while (i<results_raw[idx].score)
+			{
+				score_display=score_display+" ⭐";
+				i=i+1;
+			}
+			score_display=score_display.trim()
+			results_section.innerHTML=results_section.innerHTML+"\n\t\t\t\t\t<div class=\"thing\">\n"+elem_text+"\n\t\t\t\t\t<p>"+score_display+"</p>\n\t\t\t\t\t</div>";
 			idx++;
 		};
 
@@ -101,3 +110,8 @@ function search_mode_switch()
 		elem_page_all["style"].display="none";
 	}
 };
+
+//😐
+//👌
+//⭐
+//✅
